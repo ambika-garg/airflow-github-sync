ba7eaee7dfc242ba5f58808bade63570eac21f9b
Ambika Garg <ambika.garg@growthjockey.com>
2023-08-22 16:17:35 -0400

    Variables

--- dags/adf.py
+++ dags/adf.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-
+from airflow.models import Variable
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
@@ -18,5 +18,10 @@ with DAG(dag_id="demo_one", start_date=datetime(2023, 8, 15), schedule="0 0 * * 
     def gitsync():
         print("gitsync")
 
+    @task()
+    def printVar():
+        foo = Variable.get("foo")
+        print(foo)
+
     # Set dependencies between tasks
-    hello >> airflow() >> gitsync()
+    hello >> airflow() >> gitsync() >> printVar()
