257341d72d6a2749aedf532ad831d96606a99761
ambika-garg <ambikagarg1101.com>
2023-09-14 16:05:07 -0400

    Added requirements.txt

--- dags/adf.py
+++ dags/adf.py
@@ -1,5 +1,4 @@
 from datetime import datetime
-from airflow.models import Variable
 from airflow import DAG
 from airflow.decorators import task
 from airflow.operators.bash import BashOperator
@@ -10,7 +9,7 @@ with DAG(dag_id="demo_one", start_date=datetime(2023, 8, 15), schedule="0 0 * * 
     # Tasks are represented as operators
     hello = BashOperator(task_id="hello", bash_command="echo hello")
 
-    version = BashOperator(task_id="Check_python_version", bash_command="echo python --version")
+    version = BashOperator(task_id="Check_python_version", bash_command="python --version")
     
     # Set dependencies between tasks
     hello >> version
