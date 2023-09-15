249c731e48ec737192ee789168b9e653d165f71b
ambika-garg <ambikagarg1101.com>
2023-09-15 17:01:44 -0400

    Added dagbag

--- tests/load_dags_in_dagbag.py
+++ tests/load_dags_in_dagbag.py
@@ -1,5 +1,6 @@
 from airflow.models import DagBag
 
+
 def test_import_dags():
-    dagbag = DagBag(dag_folder = "dags", include_examples = False)
+    dagbag = DagBag(dag_folder="dags", include_examples=False)
     assert not dagbag.import_errors
