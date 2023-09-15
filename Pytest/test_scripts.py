e8944b3b4fa0c70dff65461e14ce50e2f8604089
ambika-garg <ambikagarg1101.com>
2023-09-15 13:51:40 -0400

    Test with pytest

---
+++ Pytest/test_scripts.py
@@ -0,0 +1,18 @@
+import airflow
+import os
+import mock
+import pytest
+from airflow.models import DagBag
+
+@pytest.fixture(scope='module')#scope increased so that dag loading happens once
+@mock.patch.dict(os.environ, {"key":"value"})#mocking os vars 
+def dag_bag():
+  # Create an instance of the `DagBag` class
+  dagbag = DagBag(dag_folder='dags', include_examples=False)
+  # Return the DagBag instance
+  return dagbag
+
+def test_dags_load_with_no_errors(dag_bag):
+    print(dag_bag.dags)
+    assert dag_bag.dags is not None
+    assert len(dag_bag.import_errors) == 0
--- dags/adf.py
+++ dags/adf.py
@@ -1,6 +1,5 @@
 from datetime import datetime
 from airflow import DAG
-from airflow.decorators import task
 from airflow.operators.bash import BashOperator
 
 # A DAG represents a workflow, a collection of tasks
@@ -10,6 +9,6 @@ with DAG(dag_id="demo_one", start_date=datetime(2023, 8, 15), schedule="0 0 * * 
     hello = BashOperator(task_id="hello", bash_command="echo hello")
 
     version = BashOperator(task_id="Check_python_version", bash_command="python --version")
-    
+
     # Set dependencies between tasks
     hello >> version
