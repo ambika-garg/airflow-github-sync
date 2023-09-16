b96c0ed7726e85af95b5578c5ae405c5b4ea4e26
ambika-garg <ambikagarg1101.com>
2023-09-15 23:14:45 -0400

    Changed name main to dev branch

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -1,9 +1,6 @@
 from datetime import datetime
 from airflow.operators.bash import BashOperator
-# from airflow.executors.debug_executor import DebugExecutor
-# from airflow.utils.state import State
 from airflow.models import DAG
-# , DagRun, TaskInstance
 
 
 def test_dag():
@@ -19,22 +16,3 @@ def test_dag():
         # Set dependencies between tasks
         hello >> version
         dag.test()
-
-        # dag.clear()
-        # dag.run(executor=DebugExecutor(),
-        #         start_date=dag.start_date, end_date=dag.start_date)
-
-        # # Validate DAG run was successful
-        # dagruns = DagRun.find(dag_id=dag.dag_id, execution_date=dag.start_date)
-        # assert len(dagruns) == 1
-        # assert dagruns[0].state == State.SUCCESS
-
-        # # Validate task states
-        # expected_task_states = {
-        #     hello: State.SUCCESS,
-        #     version: State.SUCCESS,
-        # }
-        # for task, expected_state in expected_task_states.items():
-        #     ti = TaskInstance(task, dag.start_date)
-        #     ti.refresh_from_db()
-        #     assert ti.state == expected_state
