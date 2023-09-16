ecd1257056b48c4de1968463e298c8fa4eded3a6
ambika-garg <ambikagarg1101.com>
2023-09-16 08:24:44 -0400

    Updated

--- tests/test_dags.py
+++ tests/test_dags.py
@@ -1,18 +1,27 @@
-from datetime import datetime
-from airflow.operators.bash import BashOperator
-from airflow.models import DAG
+# from datetime import datetime
+# from airflow.operators.bash import BashOperator
+# from airflow.models import DAG
+import pytest
+from airflow.models import DagBag
 
+@pytest.fixture()
+def dagbag():
+    return DagBag(dag_folder="dags")
 
-def test_dag():
+def test_dag(dagbag):
     """Validate a complete DAG"""
-    with DAG(dag_id="demo_one", start_date=datetime(2023, 8, 15), schedule="0 0 * * *") as dag:
+    # with DAG(dag_id="demo_one", start_date=datetime(2023, 8, 15), schedule="0 0 * * *") as dag:
 
-        # Tasks are represented as operators
-        hello = BashOperator(task_id="hello", bash_command="echo hello")
+    #     # Tasks are represented as operators
+    #     hello = BashOperator(task_id="hello", bash_command="echo hello")
 
-        version = BashOperator(
-            task_id="Check_python_version", bash_command="python --version")
+    #     version = BashOperator(
+    #         task_id="Check_python_version", bash_command="python --version")
 
-        # Set dependencies between tasks
-        hello >> version
-        dag.test()
+    #     # Set dependencies between tasks
+    #     hello >> version
+    #     dag.test()
+    dagIds = dagbag.dag_ids()
+    print(dagIds)
+
+
