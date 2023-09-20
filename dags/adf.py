8fdbf1bb41cbf9b9860d42f7263fb15f66e25b40
ambika-garg <ambikagarg1101.com>
2023-09-20 13:47:43 -0400

    Test for expected tags and dags

--- dags/adf.py
+++ dags/adf.py
@@ -2,21 +2,18 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 
-# A DAG represents a workflow, a collection of tasks
-with DAG(dag_id="demo_one", start_date=datetime(2023, 8, 15), schedule="0 0 * * *") as dag:
+with DAG(dag_id="airflow-ci-cd-tutorial", start_date=datetime(2023, 8, 15), schedule="0 0 * * *", tags=["tutorial", "CI/CD"]) as dag:
 
     # Tasks are represented as operators
-    task1 = BashOperator(task_id="hello", bash_command="echo hello")
-
-    version = BashOperator(task_id="Check_python_version", bash_command="python --version")
+    task1 = BashOperator(task_id="hello", bash_command="echo task1")
 
     task2 = BashOperator(task_id="task2", bash_command="echo task2")
 
     task3 = BashOperator(task_id="task3", bash_command="echo task3")
 
-    task4 = BashOperator(task_id="task4", command="echo task4")
+    task4 = BashOperator(task_id="task4", bash_command="echo task4")
 
     task5 = BashOperator(task_id="task5", bash_command="echo task5")
 
     # Set dependencies between tasks
-    task1 >> version >> task2 >> task3 >> task4
+    task1 >> task2 >> task3 >> task4
--- tests/test_dags.py
+++ tests/test_dags.py
@@ -11,8 +11,29 @@ def dagbag():
 
 
 def test_no_import_errors(dagbag):
+    """ 
+        Test Dags to contain no import errors.
+    """
     assert not dagbag.import_errors
 
+
+def test_expected_dags(dagbag):
+    """
+        Test whether expected dag Ids are present.
+    """
+    expected_dag_ids = ["airflow-ci-cd-tutorial"]
+    for dag_id in expected_dag_ids:
+        dag = dagbag.get_dag(dag_id)
+
+        assert dag is not None
+        assert dag_id == dag.dag_id
+
+def test_requires_specific_tag(dag_bag):
+    for dag_id, dag in dag_bag.dags.items():
+        try:
+            assert dag.tags.index("tutorial") >= 0
+        except ValueError:
+            assert dag.tags.index("CI/CD") >= 0
 
 # def test_dag(dagbag):
 #     """Validate a complete DAG"""
@@ -23,3 +44,5 @@ def test_no_import_errors(dagbag):
 #     for id in dagIds:
 #         dag = dagbag.get_dag(id)
 #         dag.test()
+
+
